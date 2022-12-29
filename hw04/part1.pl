5dd9da3745142f1baa17a6989ceff57b08fb7598
emircand <emircandemirel@yahoo.com>
2022-12-29 14:07:20 +0300

    hw04 - part1 - done

--- hw04/part1.pl
+++ hw04/part1.pl
@@ -8,7 +8,6 @@ room(z10, 78, [8, 9, 10, 11, 12, 13, 14, 15, 16], [],  projector, access).
 room(z12, 54, [8, 9, 10, 11, 12, 13, 14, 15, 16], [],  _, notAccess).
 room(amfi, 42, [8, 9, 10, 11, 12, 13, 14, 15, 16], [],  _, access).
 
-% occupancy(hour, course)
 
 % course(id, instructor, capacity, hours[], room, equipment, handicapped access)
 course(cse241, ysa, 180, [8, 9], z06, projector, notHandicapped).
@@ -31,13 +30,24 @@ student(4, [cse332, cse341], notHandicapped).
 
 % add new student
 addStudent(ID, Courses, Handicapped) :-
-    assert(student(ID, Courses, Handicapped)).
+    maplist(canEnroll(ID), Courses), % check if the student can be enrolled in all of the specified courses
+    assert(student(ID, Courses, Handicapped)), % add the student to the database
+    write('Student added succesfully').
+
 % add new course
 addCourse(ID, Instructor, Capacity, Hours, Room, Handicapped) :-
-    assert(course(ID, Instructor, Capacity, Hours, Room, Handicapped)).
+    assert(course(ID, Instructor, Capacity, Hours, Room, Handicapped)),
+    instructor(Instructor, Courses, _),
+    \+ member(ID, Courses), % check if the course is already in the instructor's course list
+    append(Courses, [ID], NewCourses), % add the course to the instructor's course list
+    retract(instructor(Instructor, Courses, _)), % remove the old version of the instructor's course list
+    assert(instructor(Instructor, NewCourses, _)), % add the updated version of the instructor's course list
+    write('Course added succesfully').
+
 % add new room
 addRoom(ID, Capacity, HoursList, Equipment, HAccess) :-
-    assert(room(ID, Capacity, HoursList, Equipment, HAccess)).
+    assert(room(ID, Capacity, HoursList, Equipment, HAccess)),
+    write('Room added succesfully').
 
 % Check if two lists have any elements in common
 hasCommonElem([H1|T1], [H2|T2]) :-
@@ -47,9 +57,11 @@ hasCommonElem([H1|T1], [H2|T2]) :-
 
 % Check if two course schedules overlap
 overlaps(Course1, Course2) :-
-    course(Course1, _, _, Hours1, _, _, _),
-    course(Course2, _, _, Hours2, _, _, _),
-    hasCommonElem(Hours1, Hours2).
+    course(Course1, Inst1, _, Hours1, Room1, _, _),
+    course(Course2, Inst2, _, Hours2, Room2, _, _),
+    ((Inst1 = Inst2, Room1 \= Room2, hasCommonElem(Hours1, Hours2)); 
+    (Course1 \= Course2, Room1 = Room2, hasCommonElem(Hours1, Hours2))).
+
 
 % Check if a course schedule overlaps with a room occupancy
 overlaps(Course, Room) :-
@@ -59,12 +71,18 @@ overlaps(Course, Room) :-
 
 % Check if a scheduling conflict exists
 schedulingConflict(Course1, Course2) :-
-    overlaps(Course1, Course2);
-    overlaps(Course2, Course1).
+    (overlaps(Course1, Course2)) ->
+        (course(Course1, _, _, Hours1, _, _, _),
+         course(Course2, _, _, Hours2, _, _, _),
+         format('Scheduling conflict detected between courses ~w (~w) and ~w (~w)', [Course1, Hours1, Course2, Hours2]));
+    true.
 
+% Check if a scheduling conflict exists
 schedulingConflict(Course, Room) :-
-    overlaps(Course, Room);
-    overlaps(Room, Course).
+    (overlaps(Course, Room)) ->
+        (course(Course, _, _, Hours, _, _, _),
+         format('Scheduling conflict detected between course ~w (~w) and room ~w', [Course, Hours, Room]));
+    true.
 
 % Check which room can be assigned to a given class. 
 % Find a suitable room for a given class
@@ -77,14 +95,11 @@ findRoom(Course, Room) :-
 findClasses(Room, Classes) :-
     findall(Course, (course(Course, _, _, _, Room, _, _)), Classes).
 
-
 % Check whether a student can be enrolled to a given class.
 canEnroll(Student, Course) :-
-    student(Student, _, _),
-    course(Course, _, Capacity, _, _, _, _),
-    findall(S, student(S, Course, _), Students),
-    length(Students, NumStudents),
-    NumStudents < Capacity.
+    student(Student, _, Handicapped),
+    course(Course, _, _, _, _, _, CourseHandicapped),
+    Handicapped = CourseHandicapped.
 
 % Check which classes a student can be assigned.
 % Find the classes that a student can be assigned to
